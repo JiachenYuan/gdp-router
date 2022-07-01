38d206c82037dfa040a1be3b2cd4bf2e997115c0
Jiachen Yuan <yuanjiachen2000@gmail.com>
2022-07-01 05:59:11 +0000

    runtime config added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-// use capsule::config::RuntimeConfig;
-// use capsule::config::MempoolConfig;
+use std::fs;
+use capsule::{config::RuntimeConfig, Runtime, PortQueue, batch::Pipeline};
+
 
 fn main() {
     println!("Hello, world!");
+    let path = "runtime_config.toml";
+    let content = fs::read_to_string(path).unwrap();
+    let config: RuntimeConfig = toml::from_str(&content).unwrap();
+    
+    let runtime: Runtime = Runtime::build(config).unwrap();
+
+    runtime.add_pipeline_to_port("eth0", installer)
+
+    
     
 
+}
+
+fn installer(port_queue: PortQueue) -> impl Pipeline {
+    
 }
