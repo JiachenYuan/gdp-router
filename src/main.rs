2f3d593149ad5d9038079e51b6f401acf3a991bc
JiachenYuan <yuanjiachen2000@gmail.com>
2022-07-04 23:45:12 -0700

    trying ping example

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,54 @@
-use std::fs;
-use capsule::{config::RuntimeConfig, Runtime, PortQueue, batch::Pipeline};
+use anyhow::Result;
+use capsule::batch::{Batch, Pipeline, Poll};
+use capsule::config::load_config;
+use capsule::packets::icmp::v4::{EchoReply, EchoRequest};
+use capsule::packets::ip::v4::Ipv4;
+use capsule::packets::{Ethernet, Packet};
+use capsule::{Mbuf, PortQueue, Runtime};
+use tracing::{debug, Level};
+use tracing_subscriber::fmt;
 
+fn reply_echo(packet: &Mbuf) -> Result<EchoReply> {
+    let reply = Mbuf::new()?;
 
-fn main() {
-    println!("Hello, world!");
-    let path = "runtime_config.toml";
-    let content = fs::read_to_string(path).unwrap();
-    let config: RuntimeConfig = toml::from_str(&content).unwrap();
-    
-    let runtime: Runtime = Runtime::build(config).unwrap();
+    let ethernet = packet.peek::<Ethernet>()?;
+    let mut reply = reply.push::<Ethernet>()?;
+    reply.set_src(ethernet.dst());
+    reply.set_dst(ethernet.src());
 
-    runtime.add_pipeline_to_port("eth0", installer)
+    let ipv4 = ethernet.peek::<Ipv4>()?;
+    let mut reply = reply.push::<Ipv4>()?;
+    reply.set_src(ipv4.dst());
+    reply.set_dst(ipv4.src());
+    reply.set_ttl(255);
 
-    
-    
+    let request = ipv4.peek::<EchoRequest>()?;
+    let mut reply = reply.push::<EchoReply>()?;
+    reply.set_identifier(request.identifier());
+    reply.set_seq_no(request.seq_no());
+    reply.set_data(request.data())?;
+    reply.reconcile_all();
 
+    debug!(?request);
+    debug!(?reply);
+
+    Ok(reply)
 }
 
-fn installer(port_queue: PortQueue) -> impl Pipeline {
-    
+fn install(q: PortQueue) -> impl Pipeline {
+    Poll::new(q.clone()).replace(reply_echo).send(q)
+}
+
+fn main() -> Result<()> {
+    let subscriber = fmt::Subscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let config = load_config()?;
+    debug!(?config);
+
+    Runtime::build(config)?
+        .add_pipeline_to_port("eth1", install)?
+        .execute()
 }
