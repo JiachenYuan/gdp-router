b951dbaf7a633c54b6ff7d2ec4ac9b36b8bd84ac
Jiachen Yuan <yuanjiachen2000@gmail.com>
2022-08-23 22:44:26 -0700

    revert

--- src/structs.rs
+++ src/structs.rs
@@ -1,5 +1,5 @@
 use anyhow::{Result, anyhow};
-use capsule::SizeOf;
+use capsule::{packets::types::u16be, SizeOf};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum GdpAction {
@@ -10,34 +10,17 @@ pub enum GdpAction {
     Noop = 4
 }
 
-#[allow(non_camel_case_types)]
-#[derive(Clone, Copy, Debug, Default)]
-#[repr(C, packed)]
-pub struct u16be(u16);
-
-impl From<u16> for u16be {
-    fn from(item: u16) -> Self {
-        u16be(u16::to_be(item))
-    }
-}
-
-impl From<u16be> for u16 {
-    fn from(item: u16be) -> Self {
-        u16::from_be(item.0)
-    }
-}
-
-impl TryFrom<u16be> for GdpAction {
+impl TryFrom<u8> for GdpAction {
     type Error = anyhow::Error;
 
-    fn try_from(v: u16be) -> Result<Self> {
+    fn try_from(v: u8) -> Result<Self> {
         match v {
-            u16be(0) => Ok(GdpAction::Ping),
-            u16be(1) => Ok(GdpAction::Register),
-            u16be(2) => Ok(GdpAction::RegisterAck),
-            u16be(3) => Ok(GdpAction::PacketForward),
-            u16be(4) => Ok(GdpAction::Noop),
-            unknown => Err(anyhow!("Unable to convert number {:?} into GDPAction. It is undefined", unknown.0)),
+            0 => Ok(GdpAction::Ping),
+            1 => Ok(GdpAction::Register),
+            2 => Ok(GdpAction::RegisterAck),
+            3 => Ok(GdpAction::PacketForward),
+            4 => Ok(GdpAction::Noop),
+            unknown => Err(anyhow!("Unable to convert number {} into GDPAction. It is undefined", unknown)),
         }
     }
 }
@@ -48,13 +31,13 @@ pub type Uuid = [u8; 16];
 #[derive(Clone, Copy, Debug, SizeOf)]
 #[repr(C)]
 pub struct GDPHeader {
-    pub action: u16be,
     pub data_len: u16be,
     pub src_gdpname: GdpName,
     pub dst_gdpname: GdpName,
     pub num_packets: i32,
     pub packet_no: i32,
-    pub uuid: Uuid // 128-bit uuid
+    pub uuid: Uuid, // 128-bit uuid
+    pub action: u8,
 }
 
 
